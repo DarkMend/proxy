88ae8c998eb51c336b040dfb03c1906410b19939
DarkMend <gindullin20@mail.ru>
2025-03-27 19:37:37 +0300

    anime

--- src/components/ui/Dropdown/Dropdown.tsx
+++ src/components/ui/Dropdown/Dropdown.tsx
@@ -5,18 +5,35 @@ import { DropdownMenu } from "radix-ui";
 import styles from "./Dropdown.module.scss";
 import { useState } from "react";
 import cn from "classnames";
+import { AnimatePresence, motion } from "framer-motion";
+
+const variants = {
+  hidden: {
+    opacity: 0,
+    transition: { duration: 0.3, ease: "easeOut" },
+  },
+  visible: {
+    opacity: 1,
+    transition: { duration: 0.3, ease: "easeOut" },
+  },
+  exit: {
+    opacity: 0,
+    transition: { duration: 0.15, ease: "easeIn" },
+  },
+};
 
 export default function Dropdown() {
   const [active, setActive] = useState(false);
 
+  const toggle = () => {
+    setActive((state) => !state);
+  };
+
   return (
     <div className={styles.dropdown}>
-      <DropdownMenu.Root>
+      <DropdownMenu.Root onOpenChange={toggle} open={active}>
         <DropdownMenu.Trigger asChild>
-          <div
-            className={styles.trigger}
-            onClick={() => setActive((state) => !state)}
-          >
+          <button className={styles.trigger}>
             <p>Текст</p>
             <div
               className={cn(styles.icon, {
@@ -25,11 +42,23 @@ export default function Dropdown() {
             >
               <SelectIcon />
             </div>
-          </div>
+          </button>
         </DropdownMenu.Trigger>
-        <DropdownMenu.Content className={styles.content}>
-          <DropdownMenu.Item className={styles.item}>1111s</DropdownMenu.Item>
-        </DropdownMenu.Content>
+        <AnimatePresence>
+          {active && (
+            <motion.div
+              variants={variants}
+              initial="hidden"
+              animate={active ? "visible" : "hidden"}
+            >
+              <DropdownMenu.Content className={styles.content}>
+                <DropdownMenu.Item className={styles.item}>
+                  1111s
+                </DropdownMenu.Item>
+              </DropdownMenu.Content>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </DropdownMenu.Root>
     </div>
   );
